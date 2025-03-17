2240241d8a6908dd902f5db000a0a9648de28e6b
Priyanka Rokhade <priyankarokhade@Priyankas-MacBook-Air.local>
2025-03-17 15:15:23 +0530

    adding code on debug

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,81 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View } from "react-native";
+import {
+  NavigationContainer,
+  useNavigationContainerRef
+} from "@react-navigation/native";
+import { useReactNavigationDevTools } from "@dev-plugins/react-navigation/build/useReactNavigationDevTools";
+import NotFoundScreen from "../+not-found";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { useApolloClientDevTools } from "@dev-plugins/apollo-client";
+//react query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+//redux plugin
+import devToolsEnhancer from "redux-devtools-expo-dev-plugin";
+import { configureStore } from "@reduxjs/toolkit";
+
+//redux plugin
+// const store = configureStore({
+//   reducer: rootReducer,
+//   enhancers: (defaultEnhancers) => [...defaultEnhancers, devToolsEnhancer()],
+// });
+
+//react query
+const queryClient = new QueryClient();
+
+//apollo client
+const client = new ApolloClient({
+  uri: "https://api.example.com/graphql",
+  cache: new InMemoryCache()
+});
 
 export default function HomeScreen() {
+  const navRef = useNavigationContainerRef();
+  useReactNavigationDevTools(navRef);
+
+  useApolloClientDevTools(client);
+
+  useReactQueryDevTools(queryClient);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={{ flex: 1 }}>
+      {/* App content */}
+      <NotFoundScreen />
+    </View>
+
+    //devtools plugins
+    // <NavigationContainer ref={navRef}>
+    //   {/* App content */}
+    //   <NotFoundScreen />
+    // </NavigationContainer>
+
+    //apollo client
+    // <ApolloProvider client={client}>{/* App content */}</ApolloProvider>;
+
+    //react query
+    // <QueryClientProvider client={queryClient}>{/* App content */}</QueryClientProvider>;
+
+    //redux
+    // <Provider store={store}>{/* App content */}</Provider>;
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
+    marginBottom: 8
   },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
-  },
+    position: "absolute"
+  }
 });
